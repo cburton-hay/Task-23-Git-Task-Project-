6a8c659e20a78125b64071abbddeccf89b602269
Catriona Burton-Hay <155481818+cburton-hay@users.noreply.github.com>
2024-03-14 16:22:16 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+"""
+create a variable for name and age.
+print using the variable
+"""
+print("Hello, world!")
+
+name = input("What's your name?")
+age = input("How old are you?")
+
+print(f"Hello {name}! You are {age}-years-old.")
+print(f"Congratulations on making it to {age}!")
